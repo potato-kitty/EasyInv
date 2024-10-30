ced2818939bb94fbec9bc98454ff540fff8c6235
potato-kitty <148569296+potato-kitty@users.noreply.github.com>
2024-10-30 16:03:10 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,17 +1,15 @@
-
-
 # here is an example of time-steps iteration
 for i, t in enumerate(tqdm(reversed(model.scheduler.timesteps), desc="DDIM Inversion")):
 
-    noise_pred = DDIM_inversion(model,latents,context,guidance_scale,t)
+    latents = DDIM_inversion(model,latents,context,guidance_scale,t)
 
-    # above is an example of inversion, it could be basically any inversion algorithm, noise_pred should be the inverse noise predicted, 
+    # above is an example of inversion, it could be basically any inversion algorithm, latents is the predicted latent for next step
     # inject following codes right after it to apply our method
 
     if (inject_steps + inject_len)*num_inference_steps > i > inject_steps*num_inference_steps:
         if i > 0:
-            latents = 0.5*latents + 0.5*last_latent
+            latents = eta*latents + (1 - eta)*last_latent
     last_latent = latents
     
-    # for SDXL, we recommand to set inject_steps = 0.85 and inject_len = 0.1 while inject_steps = 0.05 and inject_len = 0.2 for SDV1-4, 
+    # for SDXL, we recommand to set eta = 0.8, inject_steps = 0.85 and inject_len = 0.1 while eta = 0.5, inject_steps = 0.05 and inject_len = 0.2 for SDV1-4,
     # num_inference_steps is the inference steps, which is usually set to 50 
